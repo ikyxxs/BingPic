11f93483eb08ce10cba1977694f9db17e9ef9bc8
木白 <624104747@qq.com>
2020-06-24 11:27:39 +0800

    修改正则表达式

--- src/main/java/com/ikyxxs/ScheduledTasks.java
+++ src/main/java/com/ikyxxs/ScheduledTasks.java
@@ -4,15 +4,12 @@ import com.ikyxxs.util.FileUtils;
 import com.ikyxxs.util.HttpUtils;
 import com.ikyxxs.util.QiniuUtils;
 import lombok.extern.log4j.Log4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,9 +17,9 @@ import java.util.regex.Pattern;
 @Component
 public class ScheduledTasks {
 
-    private static final String url = "http://www.bing.com";
+    private static final String url = "https://cn.bing.com";
 
-    private static String regex = "(?<=url:\\s)\"/(.*)\\.jpg\"";
+    private static String regex = "/th(.*?)\\.jpg";
 
     @Scheduled(cron = "0 0 9 * * ?")        //每天9点执行
     public void reportCurrentTime() throws IOException {
@@ -35,10 +32,11 @@ public class ScheduledTasks {
 
         //通过正则表达式获取网页内的图片链接
         if (matcher.find()) {
-            String picUrl = url + matcher.group().replaceAll("\"", "");
+            String picUrl = url + matcher.group();
             String picName = FileUtils.getFileNameFromUrl(picUrl);
 
             if (!StringUtils.isEmpty(picName)) {
+                picName = picName.substring(picName.indexOf('=') + 1);
                 //下载图片到本地
                 HttpUtils.downloadPicture(picUrl, picName);
 
